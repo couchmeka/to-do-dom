a5a0609da808c755cf6f82497316b6f319299ee7
CouchMeka <meka.daniels1@gmail.com>
2022-11-09 10:50:02 -0500

    stretch goals

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let todoForm = document.querySelector('#todolist');
 let todoinput = document.querySelector('#todoinput');
-let ul = document.querySelector('ul');;
+let ul = document.querySelector('ul');
 let li = document.querySelector('li');
 let removeButton = document.querySelector('#remove');
+let removeComplete = document.querySelector('#removeAll');
 let div2 = document.querySelector('div2')
+
 
 todoForm.addEventListener('submit', function(event){
 
@@ -14,8 +16,21 @@ todoForm.addEventListener('submit', function(event){
     ul.appendChild(newLI); 
     newLI.innerText = todoinput.value;
     newLI.style.border = "dotted";
-    newLI.style.borderColor = "white"
+    newLI.style.borderColor = "purple";
+    newLI.style.background = "white"
+    newLI.style.padding ="10px"
   
+    let div2List2 = document.querySelectorAll('#div2 li')
+    for (let i = 0; i < div2List2.length; i++) {
+     
+     div2List2[i].addEventListener('click', function(){
+ 
+         div2List2[i].style.textDecoration = "line-through";
+         div2List2[i].style.opacity = ".5"
+         
+     })
+     
+    }
     
     });
     
@@ -26,13 +41,23 @@ todoForm.addEventListener('submit', function(event){
         for (let i = 0; i < div2List.length; i++){
          
             div2List[i].remove();
-            console.log('clicked')
+            
             
         }
-
-
     })
 
+        removeComplete.addEventListener('click', function(){
 
-    
+            let div2List = document.querySelectorAll('#div2 li');
+                for (let i = 0; i < div2List.length; i++){
+                 
+                    if(div2List[i].style.textDecoration === "line-through"){
+                    div2List[i].remove();
+                    }
+                    
+                }
+
+
+            })
+
     
